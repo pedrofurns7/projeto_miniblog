ae5cd709c613220a7f783ff7c1e2875bf6f04cfc
pedrofurns7 <pedroofraancisco@gmail.com>
2024-12-12 13:58:05 -0300

    [feat]: create context and setting user

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
+import "./App.css";
 
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+//hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
+//context
+import { AuthProvider } from "./context/AuthContext";
+
+//pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
+
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() =>{
+      onAuthStateChanged(auth, (user) =>{
+        setUser(user)
+      })
+  },[auth])
+
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{user}}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
