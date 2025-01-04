7378ebf5fdab7fe84772215e5af306e6c0446b0a
pedrofurns7 <pedroofraancisco@gmail.com>
2025-01-04 15:04:58 -0300

    [feat]: implements update post page and styles

---
+++ src/pages/UpdatePost/UpdatePost.js
@@ -0,0 +1,145 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
+
+import styles from "./UpdatePost.module.css";
+
+const UpdatePost = () => {
+  const { id } = useParams();
+  const { document: post } = useFetchDocument("posts", id);
+
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  useEffect(() => {
+    if (post) {
+      setTitle(post.title);
+      setBody(post.body);
+      setImage(post.image);
+
+      const textTags = post.tagsArray.join(", ");
+
+      setTags(textTags);
+    }
+  }, [post]);
+
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+
+    //validate image url
+
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL");
+    }
+
+    //create tags array
+
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    //check values
+
+    if (!title || !image || !body || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    if (formError) return;
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    navigate("/");
+  };
+
+  return (
+    <div className={styles.update_post}>
+      {post && (
+        <>
+          <h2>Editando post: {post.title}</h2>
+          <p>Altere os dados do post como desejar</p>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <span>Titulo:</span>
+              <input
+                type="text"
+                name="title"
+                required
+                placeholder="Pense num bom titulo..."
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+              />
+            </label>
+            <label>
+              <span>URL da Imagem:</span>
+              <input
+                type="text"
+                name="image"
+                required
+                placeholder="Insira uma imagem"
+                onChange={(e) => setImage(e.target.value)}
+                value={image}
+              />
+            </label>
+            <p className={styles.preview_title}>Preview da imagem atual:</p>
+            <img
+              className={styles.image_preview}
+              src={post.image}
+              alt={post.title}
+            />
+            <label>
+              <span>Conteúdo</span>
+              <textarea
+                name="body"
+                required
+                placeholder="Insira o contéudo do post!"
+                onChange={(e) => setBody(e.target.value)}
+                value={body}
+              ></textarea>
+            </label>
+            <label>
+              <span>Tags:</span>
+              <input
+                type="text"
+                name="tags"
+                required
+                placeholder="Insira as tags separadas por vírgula"
+                onChange={(e) => setTags(e.target.value)}
+                value={tags}
+              />
+            </label>
+
+            {!response.loading && <button className="btn">Editar</button>}
+            {response.loading && (
+              <button className="btn" disabled>
+                Aguarde...
+              </button>
+            )}
+            {response.error && <p className="error">{response.error}</p>}
+            {formError.error && <p className="error">{formError.error}</p>}
+          </form>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default UpdatePost;
