c7143ee3c64454a9672e78e055b83fedc36f15e2
pedrofurns7 <pedroofraancisco@gmail.com>
2025-01-04 14:58:38 -0300

    [feat]: add dashboard page and styles

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import styles from "./Dashboard.module.css";
+
+import { Link } from "react-router-dom";
+
+//hooks
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const { user } = useAuthValue();
+  const uid = user.uid;
 
-export default Dashboard
+  //post dos usuarios
+  const {
+    documents: posts,
+    loading,
+    error,
+  } = useFetchDocuments("posts", null, uid);
+
+  const { deleteDocument } = useDeleteDocument("posts");
+
+  if (loading) {
+    return <p>Carregando posts...</p>;
+  }
+
+  if (error) {
+    return <p>Erro ao carregar posts: {error}</p>;
+  }
+
+  return (
+    <div className={styles.dashboard}>
+      <h2>Dashboards</h2>
+      <p>Gerencie seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <>
+          <div className={styles.post_header}>
+            <span>Titulo</span>
+            <span>Ações</span>
+          </div>
+
+          {posts &&
+            posts.map((post) => (
+              <div key={post.id} className={styles.post_row}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                    Ver
+                  </Link>
+                  <Link
+                    to={`/posts/edit/${post.id}`}
+                    className="btn btn-outline"
+                  >
+                    Editar
+                  </Link>
+                  <button
+                    onClick={() => deleteDocument(post.id)}
+                    className="btn btn-outline btn-danger"
+                  >
+                    Excluir
+                  </button>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
